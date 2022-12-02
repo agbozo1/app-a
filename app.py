07e1246beed8f580350b440432d7e941c05376c0
agbozo1 <agbozo1@gmail.com>
2022-12-02 21:03:06 +0500

    new app

--- app.py
+++ app.py
@@ -1,130 +1,65 @@
-import cv2
-import numpy as np
-from PIL import Image, ImageOps
-import io
-
-
 import streamlit as st
-import tensorflow as tf
-np.set_printoptions(precision=1)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def emotion_classifier(image_file, model_location):
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+import pickle
 
-    #image pre-processing
-    image = ImageOps.fit(image_file, (224, 224), Image.ANTIALIAS)
-    image_array = np.asarray(image)
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-    
-    image_data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    image_data[0] = normalized_image_array
-
-    #predict
-    emotion_model = tf.keras.models.load_model(model_location)
-    prediction = emotion_model.predict(image_data)
-
-    
-    st.write(":neutral_face: - Neutral") 
-    st.progress(round(np.float(prediction[0][0]),2))
-
-    st.write(":smile: - Happy") 
-    st.progress(round(np.float(prediction[0][1]),2))
-    
-    st.write(":pensive: - Sad") 
-    st.progress(round(np.float(prediction[0][2]),2))
-
-    st.write(":angry: - Angry") 
-    st.progress(round(np.float(prediction[0][3]),2))
-
-    st.write(":anguished: - Scared") 
-    st.progress(round(np.float(prediction[0][4]),2))
-    
-    #st.progress(prediction[0][1] * 100)
-    #st.progress(prediction[0][2] * 100)
-    #st.progress(prediction[0][3] * 100)
-    #st.progress(prediction[0][4] * 100)
-    return np.argmax(prediction)
+iris_model = pickle.load(open('IrisClassifier.pkl', 'rb'))
 
 
-def take_a_shot(img):
 
-    img_check, img_buffer = cv2.imencode(".jpg", img)
-    io_bytes_buf = io.BytesIO(img_buffer)
+layout = 'centered' #wide / centered
+page_title = 'Iris App'
+page_icon = ':rose:'
 
-    decoded_img = cv2.imdecode(np.frombuffer(io_bytes_buf.getbuffer(), np.uint8), -1)
-    colorized_img = cv2.cvtColor(decoded_img, cv2.COLOR_BGR2RGB)
-    
-    pil_image=Image.fromarray(colorized_img) 
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 
-    return pil_image
+st.title(page_icon+' '+ page_title)
+#html reset
+html_style = """ 
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+"""
+st.markdown(html_style, unsafe_allow_html=True)
 
-#teachable machine
-
-st.set_page_config(layout='wide')
 with st.sidebar:
-    st.markdown("## Simple Emotion Detection App")
-    st.markdown("this project was built as part of the **#30DaysofStreamlit** in April-May 2022.")
-    st.markdown("- Liu Xiaozhi")
-    st.markdown("- Sun Xu")
-    st.markdown("- Avinash Kumar")
-    st.markdown("- Al-Samarrai Safa Shakir Awad")
-    st.markdown("**_Supervised By: Ebenezer Agbozo_**")
-    st.markdown("**_(eagbozo@urfu.ru | agbozo1@gmail.com)_**")
-    st.markdown("**_Ural Federal University_**")
-col1, col2 = st.columns(2)
-with col1:
-    st.markdown('### **Option 1: Uploader**')
-    img_upload = st.file_uploader('Use File Uploader',type=['jpg','png','jpeg'])
-
-    #IMAGE UPLOADER OPTION
-    if img_upload is not None:
-        im1 = Image.open(img_upload).convert('RGB')
-        emotion_classifier(im1, "keras_model.h5")
-        #st.image(im1)
-
-    st.write("OR")
-
-    #CAMERA OPTION
-    #camera on/off switch
-    switch = st.radio("", ("Off", "On"))
-
-    st.markdown('### **Option 2: Camera**')
-    cam = cv2.VideoCapture(0)
-    FRAME_WINDOW = st.image([])
-
-    if switch == "On":
-        st.success("Camera On") #notification
-        
-        #st_frame = st.empty()
-       
-        btn_mood = st.button("Capture Mood")
-           
-        cam_state, img = cam.read()
-        #if switch:
-        
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        FRAME_WINDOW.image(img)  
+    st.markdown("## Iris Classification App")
 
 
-        if btn_mood:    
-            #FRAME_WINDOW.image(img)
-            # encode
-            pil_image = take_a_shot(img)
-            
-            #pass image from memory into model
-            emotion_classifier(pil_image, "keras_model.h5")
-            pil_image=None
-                
-                
-    #switch off camera
-    else:
-        st.warning("Camera Off")        
-        cam.release()
+with st.container():
+    with st.form(key='my_form'):
+        st.text("Enter Details Below")
+        sep_len = st.text_input(label='Sepal Length')
+
+        sep_wid = st.text_input(label='Sepal Width')
+
+        pet_len = st.text_input(label='Petal Length')
+
+        pet_wid = st.text_input(label='Petal Width')        
+
+        submitted = st.form_submit_button(label='Predict')
     
-    #test for existence of image
-    image = cv2.imread("opencv.png")
-    if image is None:
-        st.write('No Image Located')
-    else:
-        image = Image.open("opencv.png")
+    if submitted:
+        data = [[int(sep_len), int(sep_wid), int(pet_len), int(pet_wid)]]
+        #st.text(sep_len + " " + sep_wid + " " + pet_len + pet_wid)
+        #mydata
+        st.text(data)
 
-#idea 3. web scrapper - csv
+        PredictedFlowers = iris_model.predict(data)
+        st.text(PredictedFlowers[0])
+        if PredictedFlowers == 'Iris-setosa':           
+            st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Irissetosa1.jpg/413px-Irissetosa1.jpg")
+            
+        elif PredictedFlowers == 'Iris-versicolor':
+            st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Iris_versicolor_3.jpg/800px-Iris_versicolor_3.jpg")
+
+        else:
+            st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Iris_virginica_2.jpg/330px-Iris_virginica_2.jpg")
